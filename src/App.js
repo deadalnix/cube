de58007051e8022b620a1f2daeadc422519ff9a7
Amaury Séchet <deadalnix@gmail.com>
2022-02-11 22:01:15 +0000

    Add basic react app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Hello cube!</p>
+                <a
+                    className="App-link"
+                    href="http://cube.deadalnix.me/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Start cubing!
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
