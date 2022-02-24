6bb79dbea621bfa7aa922b845a2af9859e2a55a0
Amaury Séchet <deadalnix@gmail.com>
2022-02-24 22:19:18 +0000

    Rework the index

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import type { Node } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HelloCube from "./HelloCube.js";
-import styles from "./App.scss";
 
 const App = (): Node => (
-    <div className={styles.app}>
-        <Router basename={process.env.PUBLIC_URL}>
-            <Routes>
-                <Route exact path="/" element={<HelloCube />} />
-                <Route path="*" element={<p>Poin Poin Poooiiiiiin!</p>} />
-            </Routes>
-        </Router>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+        <Routes>
+            <Route exact path="/" element={<HelloCube />} />
+            <Route path="*" element={<p>Poin Poin Poooiiiiiin!</p>} />
+        </Routes>
+    </Router>
 );
 
 export default App;
