c4d63d1f8155b2512871b2b919490bf4d42fff3a
Amaury Séchet <deadalnix@gmail.com>
2022-02-24 19:52:34 +0000

    Add a router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 // @flow
 import type { Node } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HelloCube from "./HelloCube.js";
 import styles from "./App.scss";
 
 const App = (): Node => (
     <div className={styles.app}>
-        <a
-            href="http://cube.deadalnix.me/"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            Start cubing!
-        </a>
-        <HelloCube />
+        <Router basename={process.env.PUBLIC_URL}>
+            <Routes>
+                <Route exact path="/" element={<HelloCube />} />
+                <Route path="*" element={<p>Poin Poin Poooiiiiiin!</p>} />
+            </Routes>
+        </Router>
     </div>
 );
 
--- src/HelloCube.js
+++ src/HelloCube.js
@@ -7,6 +7,13 @@ import Plls from "./cube/PLL.js";
 const HelloCube = (): Node => (
     <>
         <p>Hello cube!</p>
+        <a
+            href="http://cube.deadalnix.me/"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Start cubing!
+        </a>
         <RotatingCube size="500px" stickers={Plls.Na.stickers} />
     </>
 );
