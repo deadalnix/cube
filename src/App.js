eeebad3b50329314fe8fa9f149488ee6d38f1ecf
Amaury Séchet <deadalnix@gmail.com>
2022-03-18 15:42:46 +0000

    Redirect invalid path

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 // @flow
-import type { Node } from "react";
+import { type Node } from "react";
 import {
     BrowserRouter as Router,
     Route,
     Routes,
     Navigate,
+    useLocation,
+    useNavigate,
 } from "react-router-dom";
 
 import routes from "routes.js";
 
 import Layout from "layout/Layout";
 
-const App = (): Node => {
+import RotatingCube from "cube/svg/RotatingCube";
+
+const InvalidRoute = (): Node => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const m = location.pathname.match(/^(.*)\/index\.html$/);
+    if (m) {
+        return <Navigate to={m[1]} />;
+    }
+
+    // Redirect to home after a while.
+    setTimeout(() => navigate(""), 5000);
+
     return (
-        <Router basename={process.env.PUBLIC_URL}>
-            <Layout>
-                <Routes>
-                    {routes.map(({ path, Component }, key) => (
-                        <Route path={path} element={<Component />} key={key} />
-                    ))}
-                    <Route path="*" element={<Navigate to="/" />} />
-                </Routes>
-            </Layout>
-        </Router>
+        <div className="w-100 text-center">
+            <h1>Oooops!</h1>
+            <RotatingCube size={400} />
+            <p>We can't find what you're looking for.</p>
+        </div>
     );
 };
+
+const App = (): Node => (
+    <Router basename={process.env.PUBLIC_URL}>
+        <Layout>
+            <Routes>
+                {routes.map(({ path, Component }, key) => (
+                    <Route path={path} element={<Component />} key={key} />
+                ))}
+                <Route path="*" element={<InvalidRoute />} />
+            </Routes>
+        </Layout>
+    </Router>
+);
 
 export default App;
